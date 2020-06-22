5adca3b44c9d89436434f4ebd412d2297d4837bc
Jack <34386658+ninjawaffle2511@users.noreply.github.com>
2020-06-22 15:16:53 +1000

    Update personalinstall.sh

--- personalinstall.sh
+++ personalinstall.sh
@@ -72,24 +72,8 @@ cp /root/arch/mirrorlist /etc/pacman.d/mirrorlist
 echo "[#] Running pacstrap..."
 pacstrap /mnt base linux linux-headers linux-firmware
 
+echo "[#] Copying install script to /mnt"
+cp /root/arch/install2.sh /mnt
+
 echo "[#] chrooting into system..."
 arch-chroot /mnt
-
-echo "[#] Setting timezone to Brisbane, Australia..."
-ln -sf /usr/share/zoneinfo/Australia/Brisbane /etc/localtime
-hwclock --systohc
-cp /root/arch/locale.gen /etc/locale.gen
-locale-gen
-cp /root/arch/locale.conf /etc/locale.conf
-
-echo "[#] Please change the root password"
-passwd
-
-echo "[#] Installing networkmanager, microcode, and grub..."
-pacman -S networkmanager intel-ucode grub
-
-echo "[#] Configuring grub..."
-grub-install --target=i386-pc /dev/sda
-grub-mkconfig -o /boot/grub/grub.cfg
-
-echo "[#] Finished. Reboot system without Arch installation media"
